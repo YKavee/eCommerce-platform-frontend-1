e767b51036a5023db9da270e60b56822b2ae3279
YKavee <ykaveesenanayake@gmail.com>
2023-01-12 13:03:31 +0530

    change api calls

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,70 +1,90 @@
-import React,{useState} from 'react'
-import {auth,fs} from '../Config/Config'
-import {Link} from 'react-router-dom'
-import {useHistory} from 'react-router-dom'
+import React, { useState } from "react";
+import { auth, fs } from "../Config/Config";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
 
 export const Signup = () => {
+  const history = useHistory();
 
-    const history = useHistory();
+  const [fullName, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [fullName, setFullname]=useState('');
-    const [email, setEmail]=useState('');
-    const [password, setPassword]=useState('');
+  const [errorMsg, setErrorMsg] = useState("");
+  const [successMsg, setSuccessMsg] = useState("");
 
-    const [errorMsg, setErrorMsg]=useState('');
-    const [successMsg, setSuccessMsg]=useState('');
+  const handleSignup = async (e) => {
+    e.preventDefault();
 
-    const handleSignup=async(e)=>{
-        e.preventDefault();
-
-        try {
-			const url = "http://localhost:3000/user/signup";
-            const data = {email:email, password:password};
-			const res = await axios.post(url, data);
-			localStorage.setItem("token", JSON.stringify(res.data.token));
-            setSuccessMsg('User created successfully');
-			history.push("/login");
-		} catch (error) {
-			if (
-				error.response &&
-				error.response.status >= 400 &&
-				error.response.status <= 500
-			) {
-				setErrorMsg(error.response.data.message);
-			}
-		}
+    try {
+      const url = "http://localhost:3000/food-ordering/v1/users/signup";
+      const data = { email: email, password: password };
+      const res = await axios.post(url, data);
+      localStorage.setItem("token", JSON.stringify(res.data.token));
+      setSuccessMsg("User created successfully");
+      history.push("/login");
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setErrorMsg(error.response.data.message);
+      }
     }
+  };
 
-    return (
-        <div className='container'>
-            <br></br>
-            <br></br>
-            <h1>Sign Up</h1>
-            <hr></hr>
-            {successMsg&&<>
-                <div className='success-msg'>{successMsg}</div>
-                <br></br>
-            </>}
-            <form className='form-group' autoComplete="off" onSubmit={handleSignup}>
-                <label>Email</label>
-                <input type="email" className='form-control' required
-                 onChange={(e)=>setEmail(e.target.value)} value={email}></input>
-                <br></br>
-                <label>Password</label>
-                <input type="password" className='form-control' required
-                 onChange={(e)=>setPassword(e.target.value)} value={password}></input>
-                <br></br>
-                <div className='btn-box'>
-                    <span>Already have an account Login
-                    <Link to="login" className='link'> Here</Link></span>
-                    <button type="submit" className='btn btn-success btn-md'>SIGN UP</button>
-                </div>
-            </form>
-            {errorMsg&&<>
-                <br></br>
-                <div className='error-msg'>{errorMsg}</div>                
-            </>}
+  return (
+    <div className="container">
+      <br></br>
+      <br></br>
+      <h1>Sign Up</h1>
+      <hr></hr>
+      {successMsg && (
+        <>
+          <div className="success-msg">{successMsg}</div>
+          <br></br>
+        </>
+      )}
+      <form className="form-group" autoComplete="off" onSubmit={handleSignup}>
+        <label>Email</label>
+        <input
+          type="email"
+          className="form-control"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        ></input>
+        <br></br>
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        ></input>
+        <br></br>
+        <div className="btn-box">
+          <span>
+            Already have an account Login
+            <Link to="login" className="link">
+              {" "}
+              Here
+            </Link>
+          </span>
+          <button type="submit" className="btn btn-success btn-md">
+            SIGN UP
+          </button>
         </div>
-    )
-}
+      </form>
+      {errorMsg && (
+        <>
+          <br></br>
+          <div className="error-msg">{errorMsg}</div>
+        </>
+      )}
+    </div>
+  );
+};
